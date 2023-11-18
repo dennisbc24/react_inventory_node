d097748e1716b781dd268500e1e46883a5bb036e
dennisbc24 <dennis.bien.vida@gmail.com>
2023-11-17 23:14:04 -0500

    summary 2

--- public/javaScript/resumen.js
+++ public/javaScript/resumen.js
@@ -1,94 +1,95 @@
 //const urlBase = 'https://inventario.elwayardo.com'
-const urlBase = 'http://localhost:3000'
+const urlBase = "http://localhost:3000";
 
 function traerGanancia(url) {
-    // const urlTest = 'http://localhost:3000/api/v1/ventas'
   const cajaGrande = document.getElementById("summary");
-  //cajaGrande.innerHTML = "";
+  cajaGrande.innerHTML = "";
   window
     .fetch(url)
     .then((respuesta) => respuesta.json())
     .then((responseJson) => {
       const todosLosElementos = [];
-     
+
       responseJson.forEach((elemento) => {
         const div1 = document.createElement("div");
-        div1.className = "ultimed_sales_article";
+        div1.className = "row_summary";
 
         const branch = document.createElement("p");
         const branchApi = elemento.branch;
         branch.textContent = branchApi;
 
-        const concept = document.createElement("p")
-        concept.textContent = `Ganancia ${branchApi}`
+        const concept = document.createElement("p");
+        concept.textContent = `Ganancia ${branchApi}`;
 
         const sum = document.createElement("p");
         const sumApi = elemento.suma_revenue;
         sum.textContent = sumApi;
 
-        div1.append(branch,concept,sum);
+        div1.append(branch, concept, sum);
 
         todosLosElementos.push(div1);
 
         cajaGrande.append(...todosLosElementos);
- });
-      
+      });
+
+      const suma = responseJson.reduce((total, objeto)=>total + parseFloat(objeto.suma_revenue), 0);
+      const sumSummary = document.createElement("p");
+      sumSummary.textContent = suma;
+      cajaGrande.append(sumSummary)
     });
 }
 
-
 function traerGastos(url) {
-  // const urlTest = 'http://localhost:3000/api/v1/ventas'
-const cajaGrande = document.getElementById("summary2");
-//cajaGrande.innerHTML = "";
-window
-  .fetch(url)
-  .then((respuesta) => respuesta.json())
-  .then((responseJson) => {
-    const todosLosElementos = [];
-   
-    responseJson.forEach((elemento) => {
-      const div1 = document.createElement("div");
-      div1.className = "ultimed_sales_article";
+  const cajaGrande = document.getElementById("summary2");
+  cajaGrande.innerHTML = "";
+  window
+    .fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((responseJson) => {
+      const todosLosElementos = [];
 
-      const branch = document.createElement("p");
-      const branchApi = elemento.branch;
-      branch.textContent = branchApi;
+      responseJson.forEach((elemento) => {
+        const div1 = document.createElement("div");
+        div1.className = "row_summary";
 
-      const concept = document.createElement("p");
-      const conceptApi = elemento.concept;
-      concept.textContent = conceptApi;
+        const branch = document.createElement("p");
+        const branchApi = elemento.branch;
+        branch.textContent = branchApi;
 
-      const amount = document.createElement("p");
-      const amountApi = elemento.amount;
-      amount.textContent = amountApi;
+        const concept = document.createElement("p");
+        const conceptApi = elemento.concept;
+        concept.textContent = conceptApi;
 
-      div1.append(branch, concept, amount);
+        const amount = document.createElement("p");
+        const amountApi = elemento.amount;
+        amount.textContent = amountApi;
 
-      todosLosElementos.push(div1);
+        div1.append(branch, concept, amount);
 
-      cajaGrande.append(...todosLosElementos);
-});
-    
-  });
+        todosLosElementos.push(div1);
+
+        cajaGrande.append(...todosLosElementos);
+      });
+      const suma = responseJson.reduce((total, objeto)=>total + parseFloat(objeto.amount), 0);
+      const sumSummary = document.createElement("p");
+      sumSummary.textContent = suma;
+      cajaGrande.append(sumSummary)
+    });
 }
 
+const btnGet = document.getElementById("getSales");
 
-const btnGet = document.getElementById('getSales')
-
-btnGet.addEventListener('click', async e => {
+btnGet.addEventListener("click", async (e) => {
   e.preventDefault();
 
-  const inputMonth = document.getElementById('inputMonth')
-  
-  
-  
+  const inputMonth = document.getElementById("inputMonth");
+
   const date = inputMonth.value;
-    const year = date.substring(0,4)
-    const month = date.substring(5,7)
-    const urlInsomnia = `${urlBase}/api/v1/ventas/salesByMonth?year=${year}&month=${month}`
-    const urlInsomnia2 = `http://localhost:3000/api/v1/box/byMonth?year=${year}&month=${month}`
-    console.log(urlInsomnia);
+  const year = date.substring(0, 4);
+  const month = date.substring(5, 7);
+  const urlInsomnia = `${urlBase}/api/v1/ventas/salesByMonth?year=${year}&month=${month}`;
+  const urlInsomnia2 = `http://localhost:3000/api/v1/box/byMonth?year=${year}&month=${month}`;
+  console.log(urlInsomnia);
   traerGanancia(urlInsomnia);
   traerGastos(urlInsomnia2);
-})
+});
