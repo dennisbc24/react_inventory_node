8dcfd384ffee8d0fecc8ff01bdb36d2fab35fa7e
dennisbc24 <dennis.bien.vida@gmail.com>
2023-11-22 15:53:56 -0500

    falta gastos de luz

--- public/javaScript/resumen.js
+++ public/javaScript/resumen.js
@@ -101,7 +101,7 @@ btnGet.addEventListener("click", async (e) => {
   const month = date.substring(5, 7);
   const urlInsomnia = `${urlBase}/api/v1/ventas/salesByMonth?year=${year}&month=${month}`;
   const urlInsomnia2 = `http://localhost:3000/api/v1/box/byMonth?year=${year}&month=${month}`;
-  console.log(urlInsomnia);
+  
   traerGanancia(urlInsomnia);
   traerGastos(urlInsomnia2);
 });
@@ -112,6 +112,8 @@ function sumInputs() {
   const alquilerDep = parseFloat(document.getElementById('alquilerDep').value)
   const text = document.getElementById('subTotal')
   const sumSummary = parseFloat(document.getElementById('sumSummary').textContent)
+  const subTotal2 = document.getElementById('subTotal2')
+  const sumBils = parseFloat(document.getElementById('sumBils').textContent)
   
   if (!isNaN(alquilerB17) & !isNaN(alquilerQori) & !isNaN(alquilerDep) & !isNaN(sumSummary)) {
     
@@ -119,7 +121,7 @@ function sumInputs() {
     
     
     text.textContent = sumSummary-totalAlquiler 
-    
+    subTotal2.textContent=((sumSummary-totalAlquiler)+sumBils).toFixed(2)
   } else {
    
     text.textContent = 'ingrese valores correctos'
@@ -131,8 +133,8 @@ function sumInputs2() {
   const bonusDennis = parseFloat(document.getElementById('bonusDennis').value)
   const salaryLuz = parseFloat(document.getElementById('salaryLuz').value)
   const text = document.getElementById('total')
-  const subTotal = parseFloat(document.getElementById('subTotal').textContent)
-  const sumBils = parseFloat(document.getElementById('sumBils').textContent)
+  const subTotal = parseFloat(document.getElementById('subTotal2').textContent)
+  
   
   if (!isNaN(salaryDennis) & !isNaN(bonusDennis) & !isNaN(salaryLuz)) {
     
@@ -140,7 +142,7 @@ function sumInputs2() {
   
     
     
-    text.textContent = sumBils-totalSalaries+subTotal; 
+    text.textContent = `Utilidad EL Wayardo: S/.${(subTotal-totalSalaries).toFixed(2)}`; 
     
   } else {
    
