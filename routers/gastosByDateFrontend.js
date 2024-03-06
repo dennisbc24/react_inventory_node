eefe65cb15da9b40475df906bcc76c4c553007cf
dennisbc24 <dennis.bien.vida@gmail.com>
2024-03-05 23:20:55 -0500

    deleting templates

--- routers/gastosByDateFrontend.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.render('gastos_table')
-});
-
-module.exports = router;
--- routers/updateProductFrontend.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.render('updateProduct')
-});
-
-module.exports = router;
